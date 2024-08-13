054595b86be8b9b4b49d095503f109e6c7b1ea7b
Nicolas Martin <nicolasmartin89@gmail.com>
2024-08-12 21:00:59 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2>Bienvenidos a tu proximo viaje</h2>
+        <Button>Empieza tu viaje!</Button>
+
+      </div>
+    </>
+  )
+}
+
+export default App
