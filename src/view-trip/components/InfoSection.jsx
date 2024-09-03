f92a51c7dc5947c3a9f54cfcbc754782ceb8bc2a
Nicolas Martin <nicolasmartin89@gmail.com>
2024-09-03 18:04:39 -0300

    Fix Styles InfoSection.

--- src/view-trip/components/InfoSection.jsx
+++ src/view-trip/components/InfoSection.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function InfoSection({ trip }) {
     return (
         <div className="relative overflow-hidden rounded-2xl mt-8 px-4 lg:px-8">
+            {/* Imagen de fondo */}
             <div className="relative">
                 <img
                     src="/public/22807.jpg"
@@ -12,14 +13,19 @@ function InfoSection({ trip }) {
                 <div className="absolute inset-0 bg-black bg-opacity-40 rounded-2xl"></div>
             </div>
 
-            <div className="my-6 flex flex-col gap-4 px-5">
-                <h2 className="font-semibold text-2xl md:text-3xl lg:text-4xl text-gray-800 z-10">
+            {/* Contenido principal */}
+            <div className="mt-6 px-5">
+                <h2 className="font-semibold text-2xl md:text-3xl lg:text-4xl text-white relative z-10">
                     {trip?.userSelection?.location?.label}
                 </h2>
 
-                <div className="flex flex-wrap gap-3 text-white z-10">
+                {/* Burbujas de información */}
+                <div className="flex flex-wrap justify-center gap-4 mt-4 z-10">
                     {['numberOfDays', 'people', 'budget'].map((item, index) => (
-                        <div key={index} className="p-2 px-4 bg-orange-500 rounded-full text-white text-base md:text-lg lg:text-xl transition duration-300 hover:bg-orange-600">
+                        <div 
+                            key={index} 
+                            className="flex items-center justify-center min-w-[150px] p-4 bg-orange-500 rounded-full text-white text-sm md:text-base lg:text-lg transition duration-300 hover:bg-orange-600 shadow-md"
+                        >
                             {item === 'numberOfDays' && `📅 Días de viaje: ${trip?.userSelection?.numberOfDays}`}
                             {item === 'people' && `🚀 N° Personas: ${trip?.userSelection?.people}`}
                             {item === 'budget' && `💰 Presupuesto elegido: ${trip?.userSelection?.budget}`}
